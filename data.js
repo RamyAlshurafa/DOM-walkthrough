03f2109c5b882f7714fd784a59ce9836ae1c7ab5
ramyalshurafa <ramyshurafa@hotmail.com>
2018-07-12 13:51:19 +0300

    workshop files

---
+++ data.js
@@ -0,0 +1,271 @@
+var data =[
+{
+created_at: "Wed Jul 11 21:11:11 +0000 2018",
+text: "Stop fighting about breastfeeding and face this reality | By Gayle Tzemach Lemmon via @CNNOpinion https://t.co/OIUfUrar9a https://t.co/IorVkGqybH",
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png",
+post_image: "http://pbs.twimg.com/media/Dh2of4KW0AgViy5.jpg"
+},
+{
+created_at: "Wed Jul 11 21:09:14 +0000 2018",
+text: "RT @TheLeadCNN: A Russian internet company with links to the Kremlin was among the firms to which Facebook gave an extension which allowed…",
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png"
+},
+{
+created_at: "Wed Jul 11 21:06:07 +0000 2018",
+text: "Disney is already the king of the box office. And if the company's $71.3 billion deal for most of 21st Century Fox goes through, it will add even more big-name franchises — including Deadpool, the X-Men and Avatar — to its roster. https://t.co/mQED6kEE87 https://t.co/Ude490tqWQ",
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png",
+post_image: "http://pbs.twimg.com/media/Dh2nVrLXUAIEE8_.jpg"
+},
+{
+created_at: "Wed Jul 11 21:01:05 +0000 2018",
+text: "Sears is laying off 200 more people, including 150 in its Illinois headquarters https://t.co/fiYUDCOZqZ https://t.co/SuDCUlPJwp",
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png",
+post_image: "http://pbs.twimg.com/media/Dh2mL7bXUAEepEF.jpg"
+},
+{
+created_at: "Wed Jul 11 20:58:18 +0000 2018",
+text: 'RT @TheLeadCNN: "Dad, where were you?" Josue Rodriguez describes what it was like reuniting with his 3-year-old son after being separated f…',
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png"
+},
+{
+created_at: "Wed Jul 11 20:56:07 +0000 2018",
+text: 'Sen. Bob Menendez said he believes it would be "enormously dangerous" if President Trump were to meet alone with Russian President Vladimir Putin https://t.co/dkDpxi9or3 https://t.co/UVjZfSsOBI',
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png",
+post_image: "http://pbs.twimg.com/media/Dh2lDO0WAAAPKcO.jpg"
+},
+{
+created_at: "Wed Jul 11 20:51:21 +0000 2018",
+text: "Allies wonder if the West can withstand the Trump presidency | Analysis by @stcollinson https://t.co/Op5XKyqpm5 https://t.co/cWi16VoXrH",
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png",
+post_image: "http://pbs.twimg.com/media/Dh2j9I7W0AAOrj8.jpg"
+},
+{
+created_at: "Wed Jul 11 20:48:28 +0000 2018",
+text: "Fans cheer as Croatia's win against England puts it through to its first ever #WorldCup final, where it will face France https://t.co/DP8zkiyykT https://t.co/Leka1INGfi",
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png",
+post_image: "http://pbs.twimg.com/media/Dh2jDmpUwAABWQ-.jpg"
+},
+{
+created_at: "Wed Jul 11 20:41:03 +0000 2018",
+text: "Blood pressure linked to lesions, signs of Alzheimer's in autopsied brains, new study finds https://t.co/PjHIh5ye7C https://t.co/OsHsLU8im9",
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png",
+post_image: "http://pbs.twimg.com/media/Dh2hmS9XcAU36LF.jpg"
+},
+{
+created_at: "Wed Jul 11 20:39:46 +0000 2018",
+text: "Croatia beats England to reach their first ever #WorldCup final. They'll face 1998 champion France on Sunday. https://t.co/N9a6b4ATDv https://t.co/sZKcyVOFb9",
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png",
+post_image: "http://pbs.twimg.com/media/Dh2hTsBW4AAWsXw.jpg"
+},
+{
+created_at: "Wed Jul 11 20:36:03 +0000 2018",
+text: "Republicans on Capitol Hill say they're considering holding former FBI lawyer Lisa Page in contempt after she did not appear for an interview under subpoena, but her lawyer maintains that she's more than willing to cooperate https://t.co/sjj65Usuxr https://t.co/lGNxWE8gJa",
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png",
+post_image: "http://pbs.twimg.com/media/Dh2gdRBW4AIaeB3.jpg"
+},
+{
+created_at: "Wed Jul 11 20:29:59 +0000 2018",
+text: 'All I ask today is for my parents to be freed immediately." With a call to #FreeTheSilvas, the daughter of a couple who were detained last week on a visit to their son-in-law at a military base made a tearful plea to US Immigration and Customs Enforcement https://t.co/SQl6GLj04r https://t.co/1QAS8nzkPV',
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png",
+post_image: "http://pbs.twimg.com/ext_tw_video_thumb/1017143804317589515/pu/img/LPRYRRZl0n4iBv-b.jpg"
+},
+{
+created_at: "Wed Jul 11 20:23:36 +0000 2018",
+text: "The Senate confirmed Brian Benczkowski to lead the Justice Department's criminal division on a near party-line vote over Democratic objections to his nomination due to ties to a prominent Russian bank https://t.co/yLUEX6Njm8",
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png"
+},
+{
+created_at: "Wed Jul 11 20:21:19 +0000 2018",
+text: 'Sen. Bob Corker bluntly warned that US influence around the world is "diminishing" and said he believes President Trumps comments about NATO have been damaging to American leadership in an apparent effort to "tear apart" a critical alliance https://t.co/McEkZ7Lxaq https://t.co/A9Sr1XlLA7',
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png",
+post_image: "http://pbs.twimg.com/media/Dh2dFZWX4AEc4-E.jpg"
+},
+{
+created_at: "Wed Jul 11 20:16:03 +0000 2018",
+text: "RT @TheLeadCNN: President Trump came out brawling in his first public comments ahead of NATO's annual summit, accusing Germany — a close US…",
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png"
+},
+{
+created_at: "Wed Jul 11 20:14:35 +0000 2018",
+text: "America is falling far behind on key world goals | By Jeffrey Sachs via @CNNOpinion https://t.co/favnT0yWQD https://t.co/BKKpli9ooQ",
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png",
+post_image: "http://pbs.twimg.com/media/Dh2bipJXUAEwRya.jpg"
+},
+{
+created_at: "Wed Jul 11 20:13:57 +0000 2018",
+text: 'Little People, Big World" star Jeremy Roloff has announced that he and his wife Audrey are leaving the reality show https://t.co/FvtwzC9IwA https://t.co/xRp7I6wCcF',
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png",
+post_image: "http://pbs.twimg.com/media/Dh2bZZbWsAEYVCU.jpg"
+},
+{
+created_at: "Wed Jul 11 20:12:30 +0000 2018",
+text: "RT @CNNnewsroom: After President Trump said his meeting with Russian President Putin may be the “easiest” one, Senator Menendez says, “Some…",
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png"
+},
+{
+created_at: "Wed Jul 11 20:11:21 +0000 2018",
+text: "Trade war? Here's what investors should really be worried about https://t.co/94PnTJZye6 https://t.co/XRSxAIIeol",
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png",
+post_image: "http://pbs.twimg.com/media/Dh2azaxXcAIo6r_.jpg"
+},
+{
+created_at: "Wed Jul 11 20:09:22 +0000 2018",
+text: "RT @CNNMoney: Dow sinks 219 points, or 0.9%, on trade war worries. S&amp;P 500 loses 0.7%, snapping four-day win streak. US oil plunges by 5%,…",
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png"
+},
+{
+created_at: "Wed Jul 11 20:07:01 +0000 2018",
+text: "The Duke and Duchess of Sussex were greeted by fans in Dublin Ireland, during their first official international visit. https://t.co/si5i9Nmiwq https://t.co/TQ4uUNRnFc",
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png",
+post_image: "http://pbs.twimg.com/amplify_video_thumb/1017137660026998785/img/U5C-JdbFGe6Ifvdl.jpg"
+},
+{
+created_at: "Wed Jul 11 20:01:53 +0000 2018",
+text: "Parkland, Florida, parents and students filed suit against Broward County, the sheriff, his deputies and the school superintendent in connection with the February 14 massacre at Marjory Stoneman Douglas High School https://t.co/fxY0M070gM https://t.co/KmV4Rxbw0y",
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png",
+post_image: "http://pbs.twimg.com/media/Dh2YowSU8AAJ13g.jpg"
+},
+{
+created_at: "Wed Jul 11 19:57:04 +0000 2018",
+text: "Violent protests in Uganda have forced the government to review a controversial tax imposed on social media use in the country https://t.co/6x6wzmW0pj https://t.co/eP021SLinC",
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png",
+post_image: "http://pbs.twimg.com/media/Dh2XiKMV4AAW7Xh.jpg"
+},
+{
+created_at: "Wed Jul 11 19:49:53 +0000 2018",
+text: 'In his first public appearance since taking over as the acting head of the Environmental Protection Agency, Andrew Wheeler told employees that he was "proud of the work" he did as a coal industry lobbyist https://t.co/SRyB8VR9R8 https://t.co/XN9iFNLUVd',
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png",
+post_image: "http://pbs.twimg.com/media/Dh2V46xVAAA6fB7.jpg"
+},
+{
+created_at: "Wed Jul 11 19:37:09 +0000 2018",
+text: "Tickets for Sean Spicer's book launch party go for as much as $1,000 https://t.co/nkQJEv8CZR https://t.co/HMfbsaK9F4",
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png",
+post_image: "http://pbs.twimg.com/media/Dh2S-b0WkAAf6_r.jpg"
+},
+{
+created_at: "Wed Jul 11 19:36:07 +0000 2018",
+text: "Papa John's founder John Schnatter apologizes for using the N-word on a conference call in May https://t.co/MgcJZ8QbJU https://t.co/LKRvcRvjUm",
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png",
+post_image: "http://pbs.twimg.com/media/Dh2SvSwVMAAs233.jpg"
+},
+{
+created_at: "Wed Jul 11 19:31:34 +0000 2018",
+text: "Remarkable video shows the rescue operation to bring members of the young soccer team out of Thailand's Tham Luang cave https://t.co/SgfWVugA1b https://t.co/0b802J9IhT",
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png",
+post_image: "http://pbs.twimg.com/ext_tw_video_thumb/1017129173477969921/pu/img/93hV2z8BuoMDn4Y-.jpg"
+},
+{
+created_at: "Wed Jul 11 19:25:46 +0000 2018",
+text: "The British Association of Sexual Health and HIV has published new treatment guidelines in an attempt to prevent the sexually transmitted infection mycoplasma genitalium, also known as MGen or MG, from developing into a superbug. https://t.co/YubmYMUa8o",
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png"
+},
+{
+created_at: "Wed Jul 11 19:24:57 +0000 2018",
+text: "The Senate took a bipartisan swipe at President Donald Trump, overwhelmingly approving a motion of support for NATO https://t.co/0LhPyXNfGJ https://t.co/BFzEQRxOu3",
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png",
+post_image: "http://pbs.twimg.com/media/Dh2QLmNW4AASPgg.jpg"
+},
+{
+created_at: "Wed Jul 11 19:14:19 +0000 2018",
+text: "In an experiment, Australian scientists have successfully wiped out more than 80% of disease-carrying mosquitoes in trial locations across north Queensland https://t.co/hE94bXzt36",
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png"
+},
+{
+created_at: "Wed Jul 11 19:02:26 +0000 2018",
+text: "Trump's policies boost the for-profit detention business. But the migrants at this facility say they feel coerced into $1 a day labor and left hungry. https://t.co/noxEYBZWiB",
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png"
+},
+{
+created_at: "Wed Jul 11 18:56:45 +0000 2018",
+text: "Listen to Maggie Gyllenhaal and over 30 other Hollywood stars read an affidavit from a mother, known as Mirian, who was separated from her 18-month-old son at the US-Mexico border. https://t.co/OzwLZkkVko Read Mirian's account via @CNNOpinion: https://t.co/JZRNCL9J1X https://t.co/tjzV9hUeEu",
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png",
+post_image: "http://pbs.twimg.com/media/Dh2I0rfVAAAwd_o.jpg"
+},
+{
+created_at: "Wed Jul 11 18:50:02 +0000 2018",
+text: "These grandparents went to visit their soldier son-in-law at an Army base on July 4 and were turned over to ICE https://t.co/0XzIKrOUBP https://t.co/a2DnbdA3z2",
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png",
+post_image: "http://pbs.twimg.com/media/Dh2IMQ6X0AARAun.jpg"
+},
+{
+created_at: "Wed Jul 11 18:40:08 +0000 2018",
+text: 'The US embassy has taken the unusual step of issuing a warning to American citizens in the UK to "keep a low profile" during the visit of President Trump https://t.co/U4FCBN3QT2 https://t.co/k3WZndK4Jd',
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png",
+post_image: "http://pbs.twimg.com/media/Dh2F7WnWAAAiPRK.jpg"
+},
+{
+created_at: "Wed Jul 11 18:32:38 +0000 2018",
+text: "The Cheesemonger Invitational began as an industry event for producers, importers and sellers of cheese. Its founder is a part-time rapper who likes to wear a cow costume. Now, it's become a full-on biannual festival. https://t.co/72YeS89Is0 | via @CNNTravel https://t.co/gnCs9VCP2f",
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png",
+post_image: "http://pbs.twimg.com/amplify_video_thumb/1017037667157008385/img/4hm-WO0qlzeUISa9.jpg"
+},
+{
+created_at: "Wed Jul 11 18:30:16 +0000 2018",
+text: "A sheriff's deputy who found a baby partially buried but alive in the mountains of Montana over the weekend says he lost his composure when he found the infant -- and had just been on the verge of giving up hope the boy could be rescued. https://t.co/svoarkk5tQ https://t.co/I7wjrLmk38",
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png",
+post_image: "http://pbs.twimg.com/media/Dh2DqtqX4AUVpwy.jpg"
+},
+{
+created_at: "Wed Jul 11 18:20:03 +0000 2018",
+text: "An Australian doctor who played a key role in rescuing 12 young Thai soccer players and their coach learned afterward that his father had died during the rescue effort https://t.co/j3tbjsRHUb",
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png"
+},
+{
+created_at: "Wed Jul 11 18:10:06 +0000 2018",
+text: "A week after a 91-year-old Mexican man says he was beaten with a concrete block in Southern California, a woman has been arrested in connection with the assault, authorities said https://t.co/j0V8EiC64i https://t.co/Txea8xpXxL",
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png",
+post_image: "http://pbs.twimg.com/media/Dh1_DXSXUAAI8bt.jpg"
+},
+{
+created_at: "Wed Jul 11 18:00:01 +0000 2018",
+text: 'Drakes "Scorpion" album set a record by posting seven simultaneous singles on Billboards Top 10. That toppled The Beatles record of five singles set in 1964. https://t.co/XnwIcbXB5N https://t.co/8Wn8Y6l6hJ',
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png",
+post_image: "http://pbs.twimg.com/amplify_video_thumb/1016795255549464576/img/Es9irT5DfRNNZmvM.jpg"
+},
+{
+created_at: "Wed Jul 11 17:53:17 +0000 2018",
+text: "The hypocrisy at the heart of Trump's rancher pardon | By Peniel Joseph via @CNNOpinion https://t.co/PkzAbNYW0I https://t.co/6VGzlngzjN",
+profile_banner_url: "https://pbs.twimg.com/profile_banners/759251/1508752874",
+profile_image_url: "http://pbs.twimg.com/profile_images/508960761826131968/LnvhR8ED_normal.png",
+post_image: "http://pbs.twimg.com/media/Dh17M8EXUAI4LVW.jpg"
+}]
---
+++ main.js
@@ -0,0 +1,35 @@
+var form = document.getElementById('form')
+var searchInput = document.getElementById('searchInput')
+var container = document.getElementById('container')
+var filteredArray = []
+form.addEventListener('submit', function(event) {
+
+  //clear the old result
+
+  // get input value and filter data to match the search
+
+
+  //render the results
+  renderData(filteredArray)
+})
+
+
+// this function filters the data
+function searchInArray(array, word) {
+
+}
+
+
+// this function to render the results
+function renderData(array) {
+
+}
+
+//this function return the image url from the array
+function getImgUrl(array, index) {
+}
+
+//this function removes all children elements from the container div
+function clearResults() {
+
+}
